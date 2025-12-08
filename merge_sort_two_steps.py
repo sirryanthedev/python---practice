1e0a4daf30fbd525e1d31f08c6f45600c3ad25bc
Ryan <ryanvermeulen08@outlook.com>
2025-12-08 12:49:36 +0100

    merge sort in two steps rather than 1

---
+++ merge_sort_two_steps.py
@@ -0,0 +1,28 @@
+# (ex. 2.11. merge sort + merge lists)
+
+def merge_sort(array: list):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge_lists(left, right)
+
+def merge_lists(arr1: list, arr2: list):
+    i = j = 0
+    merged = []
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            merged.append(arr1[i])
+            i += 1
+        else:
+            merged.append(arr2[j])
+            j += 1
+
+    merged.extend(arr1[i:])
+    merged.extend(arr2[j:])
+
+    return merged
