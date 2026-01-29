976a6721199eb147352c10a3544b6243cd00080b
Ryan <ryanvermeulen08@outlook.com>
2026-01-29 20:13:02 +0100

    add raw merge sort implementation without comments

---
+++ recursion/merge_sort.py
@@ -0,0 +1,23 @@
+def merge_sort(lst: list) -> list:
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    return merge(left, right)
+
+def merge(left, right) -> list:
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+# print(merge_sort([2,5,1,4,3]))
